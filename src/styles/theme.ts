2ea77906d5519db7e537af1528e6a79b3bd423c1
Frederico Velho <fredericovcoelho@gmail.com>
2021-05-11 12:06:47 -0300

    SignIn page + Input refactoring into component

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,10 @@
 import { extendTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
+  fonts: {
+    heading: "Roboto",
+    body: "Roboto",
+  },
   colors: {
     gray: {
       "900": "#181B23",
