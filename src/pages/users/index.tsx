68180951ec50c009820d1d15bb50398d0bcc107a
Frederico Velho <fredericovcoelho@gmail.com>
2021-06-01 18:28:39 -0300

    add refetch spinner

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -9,7 +9,7 @@ import { Sidebar } from "../../components/SideBar";
 
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
+  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
     const response = await fetch('http://localhost:3000/api/users');
     const data = await response.json();
 
@@ -37,7 +37,11 @@ export default function UserList() {
 
         <Box flex="1" borderRadius={8} bg="gray.800" p="8" overflowX={["scroll", "auto"]}>
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+
+              { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
+            </Heading>
             
             <Link href="/users/create" passHref>
               <Button
