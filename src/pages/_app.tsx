b7d14f2822df1f2fb39efbbe9cf376f0fdae7de0
Frederico Velho <fredericovcoelho@gmail.com>
2021-06-04 16:04:17 -0300

    add user prefetch

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,16 @@
-import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools'
-import { theme } from '../styles/theme';
+import { AppProps } from 'next/app';
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
 import { makeServer } from '../services/mirage';
+import { queryClient } from '../services/queryClient';
+import { theme } from '../styles/theme';
 
 if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
-
-const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
