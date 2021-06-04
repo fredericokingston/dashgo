16356644af1a2380293ceb356f7b19dc31ba1536
Frederico Velho <fredericovcoelho@gmail.com>
2021-06-04 15:04:38 -0300

    add pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,32 +1,17 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
 import Link from 'next/link';
+import { useState } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { useQuery } from 'react-query';
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/SideBar";
-import { api } from "../../services/api";
-
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const { data } = await api.get('/users');
-
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-br', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        })
-      }
-    })
-    return users;
-  });
+  const [ page, setPage ] = useState(1);
+  
+  const { data, isLoading, isFetching, error } = useUsers(page);
 
   return (
     <Box>
@@ -77,7 +62,7 @@ export default function UserList() {
                     </Tr>
                   </Thead>
                   <Tbody >
-                    { data.map(user => {
+                    { data.users.map(user => {
                       return (
                         <Tr key={user.id}>
                           <Td px="6">
@@ -107,7 +92,12 @@ export default function UserList() {
                     })}
                   </Tbody>
                 </Table>
-                <Pagination />
+
+                <Pagination
+                  totalCountOfRegisters={data.totalCount}
+                  currentPage={page}
+                  onPageChange={setPage}
+                />
               </>
             )}
         </Box> 
